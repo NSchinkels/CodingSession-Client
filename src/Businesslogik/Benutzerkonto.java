a5f4b03e08f44126c2becdd9d4b1c574822c01d0
Crickyo <ndkcrick@gmail.com>
2015-05-09 20:49:00 +0200

    Sokoli-Test

--- src/Businesslogik/Benutzerkonto.java
+++ src/Businesslogik/Benutzerkonto.java
@@ -1,5 +1,12 @@
 package Businesslogik;
 
-public class Benutzerkonto {
-//test von niko
+import java.util.LinkedList;
+
+public abstract class Benutzerkonto {
+
+	// Christian Sokoli macht die schoensten Testkommentare die die Welt je gesehen hat!
+	private String emailAdresse;
+	private String passwort;
+	private static int id;
+	private LinkedList<Benutzerkonto> freunde;
 }
