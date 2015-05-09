8fb448340506d9fb0a23ce859549218a72b9af0c
CodingSession <n.schinkels@googlemail.com>
2015-05-09 16:53:08 +0200

    CS und PE anfang

--- src/Businesslogik/CodingSession.java
+++ src/Businesslogik/CodingSession.java
@@ -1,5 +1,20 @@
 package Businesslogik;
 
 public class CodingSession {
+//nicht im Diagramm,aber bestimmt wichtig 
+private int benutzerId;
+
+private String titel;
+
+private boolean speichern;
+//Im Moment noch als ein String,später was besseres
+private String code;
+
+public CodingSession(String titel,boolean speichern){
+	this.titel=titel;
+	this.speichern=speichern;
+}
+
+
 
 }
