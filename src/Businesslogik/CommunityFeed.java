77c96f1791c78cebe0d92740702db9d59f0379e5
CodingSession <n.schinkels@googlemail.com>
2015-05-20 16:07:23 +0200

    Erster CS

--- src/Businesslogik/CommunityFeed.java
+++ src/Businesslogik/CommunityFeed.java
@@ -1,5 +1,18 @@
 package Businesslogik;
 
 public class CommunityFeed {
+	private Beitrag[] Beitraege;
+	
+	public CommunityFeed(){
+		//hier werden noch die Sachen vom Server gezogen
+	}
+	
+	public Beitrag[] getBeitraege() {
+		return Beitraege;
+	}
 
+	public void setBeitraege(Beitrag[] beitraege) {
+		Beitraege = beitraege;
+	}
+	
 }
