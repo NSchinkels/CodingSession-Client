dcffdf15f0840a4fb1b37dc600862766010db21e
CodingSession <n.schinkels@googlemail.com>
2015-05-20 16:03:52 +0200

    Beitrag implementiert

--- src/Businesslogik/Beitrag.java
+++ src/Businesslogik/Beitrag.java
@@ -1,5 +1,32 @@
 package Businesslogik;
 
 public class Beitrag {
-
+	private CodingSession session;
+	private String betreff;
+	private String beschreibung;
+	private boolean schreibschutz;
+	public CodingSession getSession() {
+		return session;
+	}
+	public void setSession(CodingSession session) {
+		this.session = session;
+	}
+	public String getBetreff() {
+		return betreff;
+	}
+	public void setBetreff(String betreff) {
+		this.betreff = betreff;
+	}
+	public String getBeschreibung() {
+		return beschreibung;
+	}
+	public void setBeschreibung(String beschreibung) {
+		this.beschreibung = beschreibung;
+	}
+	public boolean isSchreibschutz() {
+		return schreibschutz;
+	}
+	public void setSchreibschutz(boolean schreibschutz) {
+		this.schreibschutz = schreibschutz;
+	}
 }
