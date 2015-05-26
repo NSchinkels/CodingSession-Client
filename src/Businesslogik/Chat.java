bb5b0341a3e14adc03506f1f8ff56c7efcd9af8d
CodingSession <n.schinkels@googlemail.com>
2015-05-26 13:19:07 +0200

    Erster Chat

--- src/Businesslogik/Chat.java
+++ src/Businesslogik/Chat.java
@@ -1,5 +1,14 @@
 package Businesslogik;
 
-public class Chat {
+import java.util.LinkedList;
 
+public class Chat {
+	private LinkedList<String> verlauf;
+	
+	public Chat(Kommunikation com){
+		this.verlauf=new LinkedList<String>();
+	}
+	public void schreiben(){
+		
+	}
 }
