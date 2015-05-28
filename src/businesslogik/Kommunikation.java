cccd05358b4191cecf0d78d11fb2db9210993e98
CodingSession <n.schinkels@googlemail.com>
2015-05-28 17:50:41 +0200

    Kommklasse kann langsam JMS

--- src/businesslogik/Kommunikation.java
+++ src/businesslogik/Kommunikation.java
@@ -1,8 +1,7 @@
 package businesslogik;
 
-import java.io.IOException;
-
 import javax.jms.Connection;
+import javax.jms.DeliveryMode;
 import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.Message;
@@ -12,73 +11,86 @@ import javax.jms.Session;
 import javax.jms.TextMessage;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-//hier wird sobald jms läuft sachen passiern
-//jetzt erstmal ziemlich theroitsche methoden
-	
+
+//KOMENTARE SPÄTER,ALLES NOCH WIP
 public class Kommunikation {
-	public Object test; 
-	public Kommunikation(){
-		this.test=new Object();
-		//Verbindung mit JMS
+	public Object test;
+	ActiveMQConnectionFactory connectionFactory;
+	Session session;
+	Destination destination;
+	MessageConsumer messageConsumer;
+	MessageListener listner;
+	Connection connection;
+
+	public Kommunikation() {
+		this.test = new Object();
+		// Verbindung mit JMS
+		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+		connectionFactory.setBrokerURL("tcp://localhost:61616");
 		try {
-			ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-			connectionFactory.setBrokerURL("tcp://localhost:61616");
-
-			Connection connection = connectionFactory.createConnection();
+			connection = connectionFactory.createConnection();
 			connection.start();
-
-			Session session = connection.createSession(false,
-					Session.AUTO_ACKNOWLEDGE);
-
-			Destination destination = session.createTopic("TestTopic");
-
-			String mySelector = "sender is not null AND JMSCorrelationID = 1234";
-
-			MessageConsumer messageConsumer = session
-					.createConsumer(destination);
-
-			//Message message = messageConsumer.receive(20000);
-
-	
-				MessageListener listner = new MessageListener() {
-					public void onMessage(Message message) {
-						try {
-							if (message instanceof TextMessage) {
-								TextMessage textMessage = (TextMessage) message;
-								System.out.println("Received message"
-										+ textMessage.getText() + "'");
-							}
-						} catch (JMSException e) {
-							System.out.println("Caught:" + e);
-							e.printStackTrace();
-						}
-					}
-				};
-			
-			messageConsumer.setMessageListener(listner);
-			//messageConsumer.close();
-			//session.close();
-			//connection.close();
-
+			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 		} catch (JMSException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+	}
+
+	public void veröffentliche(String code, String topic, String sender) {
+		// code an topic geschrieben
 		try {
-	        System.in.read();
-		} catch (IOException e) {
-	        e.printStackTrace();
-	    }
+			Destination destination = session.createTopic(topic);
+			javax.jms.MessageProducer producer = session
+					.createProducer(destination);
+			producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+			TextMessage textMessage = session.createTextMessage("Hallo");
+			textMessage.setStringProperty("sender", sender);
+			textMessage.setJMSCorrelationID("1234");
+			producer.send(textMessage);
+			// session.close();
+			// connection.close();
+		} catch (Exception e2) {
+
+		}
+
 	}
-	
-	public void veröffentlichCode(String code){
-		//code an topic geschrieben
+
+	public void bekomme(String topic, String selector) {
+		// hier wartet später das JMS
+		try {
+			destination = session.createTopic(topic);
+			String mySelector = selector;
+			messageConsumer = session.createConsumer(destination, mySelector);
+			// Message message = messageConsumer.receive(20000);
+			listner = new MessageListener() {
+				public void onMessage(Message message) {
+					try {
+						if (message instanceof TextMessage) {
+							TextMessage textMessage = (TextMessage) message;
+							textMessage.getText();
+						}
+					} catch (JMSException e) {
+						System.out.println("Caught:" + e);
+						e.printStackTrace();
+					}
+				}
+			};
+
+			messageConsumer.setMessageListener(listner);
+			// messageConsumer.close();
+			// session.close();
+			// connection.close();
+			test.notify();
+
+		} catch (Exception e1) {
+
+		}
 	}
-	public void neuerCode(){
-		//hier wartet später das JMS
-		test.notify();
-	}
-	public String getNeuerCode(){
+
+	public String getNeuerCode() {
 		return "TEST";
 	}
-	
+
 }
