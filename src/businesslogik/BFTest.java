2e67bd99d161ae4c5bf83b8f1558327f0e69e31d
CodingSession <n.schinkels@googlemail.com>
2015-06-17 16:33:24 +0200

    Meta inf aufgräumt

--- src/businesslogik/BFTest.java
+++ src/businesslogik/BFTest.java
@@ -10,18 +10,19 @@ public class BFTest {
 	public static void main(String args[]){
 		factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
 		EntityManager em = factory.createEntityManager();
-		/**em.getTransaction().begin();
-		//BenutzerkontoNickname konto1 = new BenutzerkontoNickname("unknown@me.org","passwortsicherheit","Mandant900");
+		em.getTransaction().begin();
+		/*BenutzerkontoNickname konto1 = new BenutzerkontoNickname("unknown@me.org","passwortsicherheit","Mandant900");
 		BenutzerkontoNickname konto2 = new BenutzerkontoNickname("unknown1@me.org","passwortsicherheit","CoolerTyp",2);
-		BenutzerkontoNickname konto3 = new BenutzerkontoNickname("unknown2@me.org","passwortsicherheit","RichtigCoolerTyp",2);
-		BenutzerkontoNickname konto4 = new BenutzerkontoNickname("unknown3@me.org","passwortsicherheit","KrasserTyp",2);
+		BenutzerkontoNickname konto3 = new BenutzerkontoNickname("unknown2@me.org","passwortsicherheit","RichtigCoolerTyp",3);
+		BenutzerkontoNickname konto4 = new BenutzerkontoNickname("unknown3@me.org","passwortsicherheit","KrasserTyp",4);
 		em.persist(konto2);
 		em.persist(konto3);
 		em.persist(konto4);
-		em.getTransaction().commit();**/
-		BenutzerkontoOriginal konto = em.find(BenutzerkontoNickname.class,"unknown1@me.org" );
+		em.getTransaction().commit();
+		*/
 		
-			System.out.println(konto.getID());
+		BenutzerkontoOriginal konto = em.find(BenutzerkontoNickname.class,"unknown1@me.org" );
+		System.out.println(konto.getID());
 
 		em.close();
 	}
